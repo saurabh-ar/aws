d0737f1c788efbce184a0f7113f55c61df5223fa
rjha <jha.rajeev@gmail.com>
2013-11-05 11:37:51 +0530

    added delay check inside ISR

--- code/arduino/sample/sketch_pushbutton/sketch_pushbutton.ino
+++ code/arduino/sample/sketch_pushbutton/sketch_pushbutton.ino
@@ -14,25 +14,37 @@ int ledPin = 13;
 volatile int state = LOW;
 volatile int counter = 0 ;
 
+volatile unsigned long irq_time ;
+volatile unsigned long last_irq_time ;
+
 void setup()
 {
     pinMode(ledPin, OUTPUT);
     // watch pin2
     attachInterrupt(0,isr_pin2,RISING);
+    last_irq_time = 0 ;
+    Serial.begin(9600);
+
 }
 
 void loop()
 {
     digitalWrite(ledPin, state);
-    delay(1000);
+    delay(5000);
+    Serial.print("counter=");
+    Serial.println(counter);
 
 }
 
 void isr_pin2()
 {
-    state = !state ;
-    counter++ ;
-    Serial.print("pin2 interrupted: ");
-    Serial.print("counter: ");
-    Serial.println(counter);
+    
+    // @imp donot use millis() for comparison
+    // millis() donot advance inside an ISR
+    irq_time = millis();    
+    if((irq_time - last_irq_time) > 250) {
+        state = !state ;
+        counter = counter +1 ;
+        last_irq_time = irq_time ;
+    }
 }
